383e22e723a460abd4d0b014b742a9df34e4dd15
Kirei <132071917+yjosh-dev@users.noreply.github.com>
2026-09-08 15:24:43 +0800

    feat: added content container

---
+++ frontend/src/components/ui/ContentContainer.tsx
@@ -0,0 +1,7 @@
+export default function ContentContainer () {
+    return (
+      <div className="w-full h-full bg-[#EDE9E6] border-dotted border-gray-300 ">
+        
+      </div>
+    )
+}
--- frontend/src/layout/root/RootDashboard.tsx
+++ frontend/src/layout/root/RootDashboard.tsx
@@ -6,13 +6,13 @@ import { rootAuth } from "../../service/api/auth/rootAuth";
 import StatusModal from "../../components/ui/StatusModal";
 import { MdError } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import ContentContainer from "../../components/ui/ContentContainer";
 
 export default function RootDashboard() {
   const [isActive, setIsActive] = useState(true);
-  
+
   return (
     <div className="min-h-screen flex bg-white">
-      
       <aside
         className={`h-dvh p-3 transition-[width] duration-300 ease-in-out ${
           isActive ? "w-[15%]" : "w-[6%]"
@@ -26,11 +26,12 @@ export default function RootDashboard() {
 
       <div className="flex-1 h-dvh flex flex-col gap-3 py-3 pr-3">
         <header className="w-full h-[10%] ">
-          <Header></Header>
+          <Header />
         </header>
-        <main className="w-full flex-1 "></main>
+        <main className="w-full flex-1 ">
+          <ContentContainer />
+        </main>
       </div>
-     
     </div>
   );
 }
