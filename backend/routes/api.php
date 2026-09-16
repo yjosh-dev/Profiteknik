8e1eb806d7d3babec99fcd31dde2c4e1d4adec21
Kirei <132071917+yjosh-dev@users.noreply.github.com>
2026-09-16 22:37:47 +0800

    feat: added request and controller for joblisting, also its  apiResource api endpoint, also added endpoint for verification of employee token

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,8 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RootAuthController;
 use App\Http\Controllers\RootAccountController;
-use App\Http\Controllers\Root\EmployeeAccountController;
 use App\Http\Controllers\EmployeeAuthController;
+
+use App\Http\Controllers\Root\EmployeeAccountController;
+
+use App\Http\Controllers\Employee\JobListingController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,15 +25,21 @@ Route::prefix('/root')->group(function () {
           Route::post('/logout', 'authLogout');
           Route::get('/me', 'verifyMe');
         });
-    }); 
+    });     
+
     //employee account related
-     Route::apiResource('employees', EmployeeAccountController::class);
+    Route::apiResource('employees', EmployeeAccountController::class);
 });
 
 Route::prefix('/employee')->group(function () {
      Route::prefix("/auth")->controller(EmployeeAuthController::class)->group(function () {
         Route::post('/login', 'authLogin' );
+        Route::middleware('auth:sanctum')->group(function () {
+           Route::get('/me', 'verifyMe');
+        });
      });
+     
+     Route::apiResource('job_listings', JobListingController::class);
 });
 
 Route::apiResource('root', RootAccountController::class);
