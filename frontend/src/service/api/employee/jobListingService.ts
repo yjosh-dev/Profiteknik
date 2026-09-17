de87dff017d196264d2dcc380b93a029dbddb867
Kirei <132071917+yjosh-dev@users.noreply.github.com>
2026-09-17 18:41:18 +0800

    feat: added api call service for job list related process

---
+++ frontend/src/service/api/employee/jobListingService.ts
@@ -0,0 +1,35 @@
+import axiosClient from "../axiosClient";
+
+export interface JobRequirementsData {
+  highest_education: string;
+  experience: number;
+}
+
+export interface ScreeningQuestionData {
+  screening_question: string;
+}
+
+export interface FormDataState {
+  job_title: string;
+  job_description: string;
+  minimum_salary: string | number;
+  maximum_salary: string | number;
+  vacant_position: number;
+  employment_type: string;
+  posted_at: string;
+  posted_until: string;
+  listed_by: string | number;
+  requirements: JobRequirementsData;
+  screening_questions: ScreeningQuestionData[];
+}
+
+export const jobListingApi = {
+  storeJobListing: async (formData: FormDataState, token: string) => {
+    return await axiosClient.post("/employee/job_listings", formData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+};
