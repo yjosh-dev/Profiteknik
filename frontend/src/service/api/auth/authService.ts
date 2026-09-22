5bc622f79f6d52b03b19b2ff288f461b7eada674
Kirei <132071917+yjosh-dev@users.noreply.github.com>
2026-09-22 21:48:56 +0800

    fix: added second argument for empty request body

--- frontend/src/service/api/auth/authService.ts
+++ frontend/src/service/api/auth/authService.ts
@@ -1,16 +1,18 @@
-import Header from "../../../components/common/Header"
-import axiosClient from "../axiosClient"
+import Header from "../../../components/common/Header";
+import axiosClient from "../axiosClient";
 
 export const Logout = {
-    rootLogout: async({token} : {token: string}) => (
-         axiosClient.post('/employee/auth/logout', {
-             headers: {Authorization: `Bearer ${token}`},
-         })
+  rootLogout: async ({ token }: { token: string }) =>
+    axiosClient.post(
+      "/employee/auth/logout",
+      {},
+      { headers: { Authorization: `Bearer ${token}` } },
     ),
 
-    employeeLogout: async({token} : {token: string}) => (
-         axiosClient.post('/employee/auth/logout', {
-             headers: {Authorization: `Bearer ${token}`},
-         })
-    )
-}
+  employeeLogout: async ({ token }: { token: string }) =>
+    axiosClient.post(
+      "/employee/auth/logout",
+      {},
+      { headers: { Authorization: `Bearer ${token}` } }, // config goes here
+    ),
+};
